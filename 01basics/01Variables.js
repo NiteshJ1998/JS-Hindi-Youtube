1b612f0e574646a71ed7eb6ca49faf7322a09ae5
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2023-10-28 21:29:51 +0000

    added variable.js file

---
+++ 01basics/01Variables.js
@@ -0,0 +1,17 @@
+const accountName = "Nitesh"
+// const value once declare cannot be re-declare 
+let accountEmail = 'nitesh@google.com'
+var accountPassword = 'fjasdkl;'
+
+accountCity = 'Mumbai'
+
+console.table([accountName, accountEmail, accountPassword, accountCity])
+
+
+
+
+/*
+Prefer not to use var 
+because of issue on block scope and fun scope
+
+*/
