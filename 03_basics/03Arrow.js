55903651259fcb2cd5709621c79b668b83dbb4fc
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2023-11-08 12:02:43 +0000

    update arrow.js

--- 03_basics/03Arrow.js
+++ 03_basics/03Arrow.js
@@ -4,6 +4,33 @@ const users ={
 
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website`) /// this keyword refer to current context 
+        console.log(this)
     }
 }
 
+// users.welcomeMessage()
+// users.username = 'vicky'
+// users.welcomeMessage()
+
+// console.log(this)
+
+// function one(){
+//     let username = 'vinayak'
+//     console.log(this)
+// }
+
+// one()
+
+
+const chai = () => {
+    let username = 'vinayak'
+    console.log(this)
+}
+
+chai()
+
+// const addTwo = (num1, num2) => (num1 + num2)// implicit return
+
+// const addTwwo = (num1, num2) =>{  ///Explict retrun
+//     return num1+ num2
+// }
