766e31cf026d15f87c5b18be14bdcd682aa77166
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2023-11-13 18:22:15 +0000

    three.js

---
+++ 05_iteration/three.js
@@ -0,0 +1,40 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+// For of loooooooooooooop
+
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+
+
+const greetings = "Nitesh Jaiswar"
+
+
+for (const greet of greetings) {
+    if (greet != ' ') {
+        console.log(greet)
+        
+    }
+    // console.log(`Each char is ${greet}`)
+    
+}
+
+// const myString = "Hello world";
+
+// for (const char of myString) {
+//   if (char !== ' ') {
+//     // console.log(char);
+//   }
+// }
+
+
+// MAPS
+
+const map = new map()
