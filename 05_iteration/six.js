7918b25bdc716da1c9706ce9e5f6c312c17ea62d
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2023-11-14 19:13:06 +0000

    six.js

---
+++ 05_iteration/six.js
@@ -0,0 +1,28 @@
+// const fruits = ["apple", "pineapple","mango","banana","kiwi"]
+
+// const values = fruits.forEach((item)=>{
+//     // console.log(item)
+//     return item
+// })
+
+// console.log(values)
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num) => {
+//     return num > 4
+// })
+
+
+const newNums = []
+
+
+// myNums.forEach((num)=>{
+//     if (num > 4){ 
+//         newNums.push(num)
+//     }
+// })
+
+
+// console.log(newNums)
